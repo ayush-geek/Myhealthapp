9a88f002f2bbb5a2f21aaba8d4dcc548452facc9
Ayush-Malviya-1 <ayushmalviya12a@gmail.com>
2022-11-14 23:47:26 +0530

    Change Target Now Working

--- app/src/main/java/com/example/myhealthapp/DashBoardFragment.java
+++ app/src/main/java/com/example/myhealthapp/DashBoardFragment.java
@@ -1,35 +1,43 @@
 package com.example.myhealthapp;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.firebase.ui.auth.AuthUI;
+import com.example.myhealthapp.network.model.DailyLimit;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-
-import org.checkerframework.checker.units.qual.A;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DashBoardFragment extends Fragment {
     private PieChart pieChart;
-
+    TextView tv_food ,tv_goal,tv_remaining;
     public DashBoardFragment() {
+
+
+
     }
 
     @Override
@@ -40,8 +48,71 @@ public class DashBoardFragment extends Fragment {
         pieChart = myV.findViewById(R.id.pie_chart);
         drawPC();
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DocumentReference docRef = db.collection("dailyLimit").document(user.getUid());
+
+        docRef.get().addOnCompleteListener((new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("IMAD", "DocumentSnapshot data: " + document.getData());
+//                            City city = documentSnapshot.toObject(City.class);
+                        DailyLimit dl=document.toObject(DailyLimit.class);
+                        tv_goal=(TextView)  myV.findViewById(R.id.goal);
+                        tv_goal.setText(String.valueOf(dl.getDaily_limit()));
+
+                        tv_food=(TextView)  myV.findViewById(R.id.food);
+                        tv_food.setText(String.valueOf(dl.getConsumption()));
+
+                        // tv_remaining=(TextView) findViewById(R.id.remaining);
+                        // tv_remaining.setText(String.valueOf(dl.getDaily_limit()-dl.getConsumption()));
+                    } else {
+                        Log.d("IMAD", "No such document");
+
+                        Map<String, Object> city = new HashMap<>();
+                        DailyLimit dlimit = new DailyLimit(2300, 0);
+
+                        docRef.set(dlimit).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("IMAD", "DocumentSnapshot written with ID: " + docRef.getId());
+
+                                tv_goal=(TextView)  myV.findViewById(R.id.goal);
+                                tv_goal.setText(String.valueOf(2300));
+
+                                tv_food=(TextView)  myV.findViewById(R.id.food);
+                                tv_food.setText(String.valueOf(0));
+
+                                // tv_remaining=(TextView) findViewById(R.id.remaining);
+                                //tv_remaining.setText(String.valueOf(dl.getDaily_limit()-dl.getConsumption()));
+                            }
+
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("IMAD", "Error adding document", e);
+                            }
+                        });
+
+                    }
+                } else {
+                    Log.d("IMAD", "get failed with ", task.getException());
+
+                    //Craete a doc
+
+
+
+                }
+            }
+        }));
         return myV;
     }
+
+
 
     private void drawPC() {
         ArrayList<PieEntry> pieEntries = new ArrayList<>();
--- app/src/main/java/com/example/myhealthapp/TargetFragment.java
+++ app/src/main/java/com/example/myhealthapp/TargetFragment.java
@@ -2,21 +2,29 @@ package com.example.myhealthapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TargetFragment extends Fragment {
+
     public TargetFragment() {
+
+
     }
 
     @Override
@@ -31,9 +39,52 @@ public class TargetFragment extends Fragment {
                 handleLogout();
             }
         });
+        Button  bt=(Button) v.findViewById(R.id.tarButton);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText ed1=(EditText)  v.findViewById(R.id.setTarget);
+              //  ed1.setText(ed1.getText());
+                int val=Integer.parseInt(ed1.getText().toString());
+                updateTarget(val);
+                ed1.setText(String.valueOf(val));
+            }
+        });
+
 
         return v;
     }
+
+
+    public void updateTarget(int val)
+    {
+//
+//        EditText ed1=(EditText)  v.findViewById(R.id.setTarget);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        db.collection("dailyLimit").document(user.getUid())
+                .update("daily_limit", val)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("IMAD","Firebase Updated");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("IMAD",e.toString());
+                    }
+                });
+
+
+
+
+
+    }
+
 
     public void handleLogout() {
         AuthUI.getInstance().signOut(requireContext()).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -47,4 +98,7 @@ public class TargetFragment extends Fragment {
             }
         });
     }
+
+
+
 }
